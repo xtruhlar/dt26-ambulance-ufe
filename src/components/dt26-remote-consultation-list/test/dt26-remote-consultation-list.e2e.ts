2644b7d179183783dd425d197c11644c1c0c8cd2
David <davidtruhlarac@gmail.com>
2026-05-05 20:25:39 +0200

    fix build retry

--- src/components/dt26-remote-consultation-list/test/dt26-remote-consultation-list.e2e.ts
+++ src/components/dt26-remote-consultation-list/test/dt26-remote-consultation-list.e2e.ts
@@ -8,12 +8,12 @@ describe('dt26-remote-consultation-list', () => {
     expect(element).not.toBeNull();
   });
 
-  it('emits entry-clicked on item click', async () => {
+  it('emits entry-clicked on add button click', async () => {
     const page = await newE2EPage();
     await page.setContent('<dt26-remote-consultation-list></dt26-remote-consultation-list>');
     const entryClicked = await page.spyOnEvent('entry-clicked');
-    const item = await page.find('dt26-remote-consultation-list >>> md-list-item');
-    await item.click();
+    const btn = await page.find('dt26-remote-consultation-list >>> .add-button');
+    await btn.click();
     expect(entryClicked).toHaveReceivedEvent();
   });
 
