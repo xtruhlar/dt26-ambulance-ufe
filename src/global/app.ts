38fe2c621ed572588c7b289d72c8aa503fed5e24
David <davidtruhlarac@gmail.com>
2026-05-06 15:18:22 +0200

    move font to app.ts

--- src/global/app.ts
+++ src/global/app.ts
@@ -3,6 +3,16 @@ import '@material/web/list/list'
 import '@material/web/list/list-item'
 import '@material/web/icon/icon'
 
-export default function() { // or export default async function()
+function loadGoogleFont(href: string) {
+  if (document.querySelector(`link[href="${href}"]`)) return;
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = href;
+  document.head.appendChild(link);
+}
+
+export default function() {
+  loadGoogleFont('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined');
+  loadGoogleFont('https://fonts.googleapis.com/css?family=Roboto:300,400,500');
   registerNavigationApi()
 }
